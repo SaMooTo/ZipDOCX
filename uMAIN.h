261564ccee2854a424221cbc935faef8b1ac392a
SaMooTo <gustovsanek@gmail.com>
2026-03-09 13:01:07 +0300

    Фул работает

---
+++ uMAIN.h
@@ -0,0 +1,33 @@
+//---------------------------------------------------------------------------
+
+#ifndef uMAINH
+#define uMAINH
+//---------------------------------------------------------------------------
+#include <System.Classes.hpp>
+#include <Vcl.Controls.hpp>
+#include <Vcl.StdCtrls.hpp>
+#include <Vcl.Forms.hpp>
+#include <Vcl.Dialogs.hpp>
+#include <Vcl.ExtCtrls.hpp>
+#include <Vcl.ComCtrls.hpp>
+//---------------------------------------------------------------------------
+class TForm1 : public TForm
+{
+__published:	// IDE-managed Components
+	TPanel *Panel1;
+	TOpenDialog *OpenDialogDOCX;
+	TButton *UNZIP;
+	TButton *CHOOSE;
+	TEdit *PATH;
+	TButton *ZIP;
+	void __fastcall CHOOSEClick(TObject *Sender);
+	void __fastcall UNZIPClick(TObject *Sender);
+	void __fastcall ZIPClick(TObject *Sender);
+private:	// User declarations
+public:		// User declarations
+	__fastcall TForm1(TComponent* Owner);
+};
+//---------------------------------------------------------------------------
+extern PACKAGE TForm1 *Form1;
+//---------------------------------------------------------------------------
+#endif
